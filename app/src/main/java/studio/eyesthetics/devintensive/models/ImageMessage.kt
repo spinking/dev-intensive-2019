3ea19e5b6249abdd963fd72a2d4a8bae2be003f5
Spinking <spinking@mail.ru>
2019-06-28 15:02:54 +0400

    test git

--- app/src/main/java/studio/eyesthetics/devintensive/models/ImageMessage.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/models/ImageMessage.kt
@@ -17,3 +17,4 @@ class ImageMessage(
     override fun formatMessage(): String = "id:$id ${from?.firstName}" +
             "${if(isIncoming) "получил" else " отправил" } изображение \"$image\"" //+ "${date.humanizeDiff()}"
 }
+//test git
