b0cef44bbf28fd60f913a42a3b12381cfb55d206
EyEsthetics <splnklng@yandex.ru>
2019-07-02 08:15:42 +0400

    fix Date.format, replace ":" to "." in date

--- app/src/main/java/studio/eyesthetics/devintensive/extensions/Date.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/extensions/Date.kt
@@ -12,7 +12,7 @@ const val MINUTE = 60 * SECOND
 const val HOUR = 60 * MINUTE
 const val DAY = 24 * HOUR
 
-fun Date.format(pattern: String = "HH:mm:ss dd:MM:yy"): String {
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
 }
