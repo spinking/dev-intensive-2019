74d369adbaeee854cd561b3b6f6ceca734d45b6f
EyEsthetics <splnklng@yandex.ru>
2019-07-02 08:07:39 +0400

    delete id in ImageMessage and TextMessage

--- app/src/main/java/studio/eyesthetics/devintensive/models/ImageMessage.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/models/ImageMessage.kt
@@ -14,6 +14,6 @@ class ImageMessage(
     date: Date = Date(),
     var image: String?
 ) : BaseMessage(id, from, chat, isIncoming, date) {
-    override fun formatMessage(): String = "id:$id ${from?.firstName}" +
+    override fun formatMessage(): String = "${from?.firstName}" +
             "${if(isIncoming) " получил" else " отправил" } изображение \"$image\" ${date.humanizeDiff()}"
 }
--- app/src/main/java/studio/eyesthetics/devintensive/models/TextMessage.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/models/TextMessage.kt
@@ -14,7 +14,7 @@ class TextMessage(
     date: Date = Date(),
     var text: String?
 ) : BaseMessage(id, from, chat, isIncoming, date) {
-    override fun formatMessage(): String = "id:$id ${from?.firstName}" +
+    override fun formatMessage(): String = "${from?.firstName}" +
     "${if(isIncoming) " получил" else " отправил" } сообщение \"$text\" ${date.humanizeDiff()}"
 
 }
