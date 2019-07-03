7ab6699cfb96f86302c6b541ea81490c9176bcca
EyEsthetics <splnklng@yandex.ru>
2019-07-03 21:41:06 +0400

    second fix string truncate

--- app/src/main/java/studio/eyesthetics/devintensive/extensions/String.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/extensions/String.kt
@@ -3,8 +3,9 @@ package studio.eyesthetics.devintensive.extensions
 import android.text.Html
 
 fun String.truncate(index: Int = 16): String {
-    if(this.length < index) return this.trim()
-    var str = this.substring(0, index)
+    var str = this
+    if(this.length < index) return this
+    str = this.substring(0, index)
     if(str.takeLast(length) == " ") str = str.substring(0, length - 1)
     return "${this.substring(0, index)}..."
 }
