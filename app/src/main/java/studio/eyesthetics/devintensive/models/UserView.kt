c27f77ab0785e682bedf5b3ed63170880788d8cd
Spinking <spinking@mail.ru>
2019-07-04 08:44:29 +0400

    fix package

--- app/src/main/java/studio/eyesthetics/devintensive/models/UserView.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/models/UserView.kt
@@ -1,8 +1,5 @@
 package ru.skillbranch.devintensive.models
 
-import studio.eyesthetics.devintensive.extensions.truncate
-import studio.eyesthetics.devintensive.extensions.stripHtml
-
 class UserView(
     val id:String,
     val fullName: String,
