762263953126e8f1c2187f7b02b012065a94f2f3
Spinking <spinking@mail.ru>
2019-07-04 13:30:42 +0400

    fix truncate

--- app/src/main/java/studio/eyesthetics/devintensive/extensions/String.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/extensions/String.kt
@@ -2,7 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(index: Int = 16) :String {
     val str = this.trim()
-    return if(str.length < index + 2) str else str.substring(0, index + 1).trim () + "..."
+    return if(str.length < index + 1) str else str.substring(0, index).trim () + "..."
 }
 
 fun String.stripHtml(): String {
--- app/src/test/java/studio/eyesthetics/devintensive/ExampleUnitTest.kt
+++ app/src/test/java/studio/eyesthetics/devintensive/ExampleUnitTest.kt
@@ -468,21 +468,11 @@ class ExampleUnitTest {
 
     @Test
     fun test_truncate() {
-
-            /* skillBranch tests */
-            assertEquals("Bender Bending Ro...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate())
-            assertEquals("Bender Bending R...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15))
-            assertEquals("A", "A     ".truncate(3))
-
-            /* additional tests */
-            assertEquals("too lo...", "   too long line with lots of spaces before".truncate(5))
-            assertEquals("too short", "too short".truncate(20))
-            assertEquals("12345", "12345".truncate(4))
-            assertEquals("1234...", "12345".truncate(3))
-            assertEquals("12345", "12345  ".truncate(4))
-            assertEquals("tab", "tab    ".truncate(4))
-            assertEquals("dots......", "dots... a lot".truncate(6))
-            assertEquals("abc", "abc    ".truncate(15))
+            assertEquals("123456...", "123456789".truncate(6))
+            assertEquals("123456789", "123456789".truncate(9))
+            assertEquals("1", "1     ".truncate(3))
+            assertEquals("Bender Bending R...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate())
+            assertEquals("Bender Bending...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15))
 
         /*Реализуй extension усекающий исходную строку до указанного числа символов (по умолчанию 16) и возвращающий
         усеченную строку с заполнителем "..." если последний символ усеченной строки является пробелом - удалить его и добавить заполнитель
