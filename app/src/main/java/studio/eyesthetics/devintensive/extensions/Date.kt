4bbc011826b9f7adb74bd82c0f5988eb0e2766a0
Spinking <spinking@mail.ru>
2019-07-08 12:50:09 +0400

    humanizeDiff refactoring

--- app/src/main/java/studio/eyesthetics/devintensive/extensions/Date.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/extensions/Date.kt
@@ -14,10 +14,6 @@ val Int.min get() = this * MINUTE
 val Int.hour get() = this * HOUR
 val Int.day get() = this * DAY
 
-val Long.asMin get() = this.absoluteValue / MINUTE
-val Long.asHour get() = this.absoluteValue / HOUR
-val Long.asDay get() = this.absoluteValue / DAY
-
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
@@ -44,22 +40,22 @@ fun Date.humanizeDiff(date: Date = Date()): String {
             in 0.sec..1.sec -> "только что"
             in 2.sec..45.sec -> "несколько секунд назад"
             in 46.sec..75.sec -> "минуту назад"
-            in 76.sec..45.min -> "${minuteAsPlurals(duration.asMin)} назад"
+            in 76.sec..45.min -> "${TimeUnits.MINUTE.plural((duration.absoluteValue / MINUTE).toInt())} назад"
             in 46.min..75.min -> "час назад"
-            in 76.min..22.hour -> "${hourAsPlurals(duration.asHour)} назад"
+            in 76.min..22.hour -> "${TimeUnits.HOUR.plural((duration.absoluteValue / HOUR).toInt())} назад"
             in 23.hour..26.hour -> "день назад"
-            in 27.hour..360.day -> "${dayAsPlurals(duration.asDay)} назад"
+            in 27.hour..360.day -> "${TimeUnits.DAY.plural((duration.absoluteValue / DAY).toInt())} назад"
             else -> "более года назад"
         }
     } else {
         when (duration) {
             in (-45).sec..0.sec -> "через несколько секунд"
             in (-75).sec..(-45).sec -> "через минуту"
-            in (-45).min..(-75).sec -> "через ${minuteAsPlurals(duration.asMin)}"
+            in (-45).min..(-75).sec -> "через ${TimeUnits.MINUTE.plural((duration.absoluteValue / MINUTE).toInt())}"
             in (-75).min..(-45).min -> "через час"
-            in (-22).hour..(-75).min -> "через ${hourAsPlurals(duration.asHour)}"
+            in (-22).hour..(-75).min -> "через ${TimeUnits.HOUR.plural((duration.absoluteValue / HOUR).toInt())}"
             in (-26).hour..(-22).hour -> "через день"
-            in (-360).day..(-26).hour -> "через ${dayAsPlurals(duration.asDay)}"
+            in (-360).day..(-26).hour -> "через ${TimeUnits.DAY.plural((duration.absoluteValue / DAY).toInt())}"
             else -> "более чем через год"
         }
     }
@@ -102,12 +98,6 @@ private fun dayAsPlurals(value: Long) = when(value.asPlurals) {
     Plurals.ONE -> "$value день"
     Plurals.FEW -> "$value дня"
     Plurals.MANY -> "$value дней"
-}
-
-private fun yearAsPlurals(value: Long) = when(value.asPlurals) {
-    Plurals.ONE -> "$value год"
-    Plurals.FEW -> "$value года"
-    Plurals.MANY -> "$value лет"
 }
 
 val Long.asPlurals
