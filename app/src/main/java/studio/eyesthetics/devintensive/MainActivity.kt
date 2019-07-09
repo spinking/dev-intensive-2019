3da7fd46e99ea59b9f974023771366090ba84102
Spinking <spinking@mail.ru>
2019-07-09 11:06:58 +0400

    hometask_3

--- app/src/main/java/studio/eyesthetics/devintensive/MainActivity.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
