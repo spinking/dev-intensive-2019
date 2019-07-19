8dd010386886045ecde05c77727f3d47c004c46b
Spinking <spinking@mail.ru>
2019-07-19 09:09:02 +0400

    add colorFilter animation

--- app/src/main/java/studio/eyesthetics/devintensive/MainActivity.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/MainActivity.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
 import android.graphics.Color
 import android.graphics.ColorFilter
 import android.graphics.PorterDuff
@@ -12,13 +14,14 @@ import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.skillbranch.devintensive.extensions.hideKeyboard
 import ru.skillbranch.devintensive.extensions.isKeyboardOpen
 
 import studio.eyesthetics.devintensive.models.Bender
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener,ValueAnimator.AnimatorUpdateListener {
     lateinit var benderImage: ImageView
     lateinit var textTxt: TextView
     lateinit var messageEt: EditText
@@ -184,11 +187,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun doIt() {
+        val prevColor = benderObj.status.color
         val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString())
         messageEt.setText("")
         val (r, g, b) = color
-        benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
-        sendBtn.setColorFilter(Color.rgb(r, g, b))
+        //benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+        animate(Color.rgb(prevColor.first, prevColor.second, prevColor.third), Color.rgb(r, g, b))
+        //sendBtn.setColorFilter(Color.rgb(r, g, b))
         textTxt.text = phrase
+    }
+
+    private fun animate(color1: Int, color2: Int) {
+        val bgAnim = ValueAnimator.ofObject(ArgbEvaluator(), color1, color2)
+        bgAnim.setDuration(600)
+        bgAnim.addUpdateListener(this)
+        bgAnim.start()
+    }
+
+    override fun onAnimationUpdate(animation: ValueAnimator?) {
+        benderImage.setColorFilter(animation?.animatedValue as Int, PorterDuff.Mode.MULTIPLY)
+        sendBtn.setColorFilter(animation?.animatedValue as Int)
     }
 }
