b9bb7c98396742196baa11cd4e87124c027a6c1c
Spinking <spinking@mail.ru>
2019-07-22 17:48:38 +0400

    add activity constraint layout

--- app/src/main/java/studio/eyesthetics/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/ui/profile/ProfileActivity.kt
@@ -38,7 +38,7 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_profile_constraint)
 
     }
 
