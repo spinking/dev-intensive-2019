09737cfb7d8e9b1a41b5e5ae0c11fd0b9358bba3
Spinking <spinking@mail.ru>
2019-07-23 09:31:05 +0400

    add edit method

--- app/src/main/java/studio/eyesthetics/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/ui/profile/ProfileActivity.kt
@@ -21,14 +21,51 @@ import ru.skillbranch.devintensive.extensions.isKeyboardOpen
 import studio.eyesthetics.devintensive.models.Bender
 
 class ProfileActivity : AppCompatActivity() {
+    companion object {
+        const val IS_EDIT_MODE = "IS_EDIT_MODE"
+    }
+
+    var isEditMode = false
+    lateinit var viewFields : Map<String, TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_constraint)
+        setContentView(R.layout.activity_profile)
+        initViews(savedInstanceState)
 
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
+    }
+
+    private fun initViews(savedInstanceState: Bundle?) {
+        viewFields = mapOf(
+            "nickname" to tv_nick_name,
+            "rank" to tv_rank,
+            "firstName" to et_first_name,
+            "lastName" to et_last_name,
+            "about" to et_about,
+            "repository" to et_repository,
+            "rating" to tv_rating,
+            "respect" to tv_respect
+        )
+
+        btn_edit.setOnClickListener {
+            isEditMode = !isEditMode
+            showCurrentMode(isEditMode)
+         }
+
+    }
+
+    private fun showCurrentMode(isEdit: Boolean) {
+        val info = viewFields.filter { setOf("firstName", "lastName", "about", "repository").contains(it.key) }
+        for((_, v) in info) {
+            v as EditText
+            v.isFocusable = isEdit
+            v.isFocusableInTouchMode = isEdit
+            v.isEnabled = isEdit
+            v.background.alpha = if(isEdit) 255 else 0
+        }
     }
 }
