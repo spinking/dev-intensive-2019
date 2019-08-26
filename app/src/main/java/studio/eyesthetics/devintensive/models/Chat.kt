007e8cc75ed1b7b4ac7cedc794a1d015549c3341
Spinking <spinking@mail.ru>
2019-08-26 11:25:11 +0400

    refactor Chat

--- app/src/main/java/studio/eyesthetics/devintensive/models/Chat.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/models/Chat.kt
@@ -1,11 +1,32 @@
 package ru.skillbranch.devintensive.models
 
+import java.util.*
+
 
 /**
  * Created by BashkatovSM on 28.06.2019
  */
 class Chat(
     val id:String,
-    val members: MutableList<User> = mutableListOf(),
-    val messages: MutableList<BaseMessage> = mutableListOf()
-)
+    val title: String,
+    val members: List<User> = listOf(),
+    var messages: MutableList<BaseMessage> = mutableListOf(),
+    var isArchived: Boolean = false
+) {
+    fun unreadableMessageCount(): Int {
+        //TODO implement me
+        return 0
+    }
+
+    private fun lastMessageDate(): Date? {
+        //TODO implement me
+        return Date()
+    }
+
+    private fun lastMessageShort(): String {
+        //TODO implement me
+        return "Сообщений нет"
+    }
+
+    private fun isSingle(): Boolean = members.size == 1
+}
