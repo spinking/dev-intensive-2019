d2a13ad94f84ba9834f7f3aedb5d8ebb053c589a
Spinking <spinking@mail.ru>
2019-08-26 11:06:15 +0400

    added LiveData extension

--- app/src/main/java/studio/eyesthetics/devintensive/extensions/LiveData.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/extensions/LiveData.kt
@@ -1,5 +1,17 @@
 package ru.skillbranch.devintensive.extensions
 
+import androidx.lifecycle.MutableLiveData
+
 /**
  * Created by BashkatovSM on 26.08.2019
  */
+
+ fun <T>mutableLiveData(defaultValue: T? = null): MutableLiveData<T> {
+    val data = MutableLiveData<T>()
+
+    if(defaultValue != null) {
+        data.value = defaultValue
+    }
+
+    return data
+ }
--- app/src/main/java/studio/eyesthetics/devintensive/viewmodels/MainViewModel.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/viewmodels/MainViewModel.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import ru.skillbranch.devintensive.extensions.mutableLiveData
 import ru.skillbranch.devintensive.models.data.ChatItem
 import ru.skillbranch.devintensive.repositories.ChatRepository
 /**
@@ -9,7 +10,7 @@ import ru.skillbranch.devintensive.repositories.ChatRepository
  */
 class MainViewModel: ViewModel() {
     fun getChatData() : LiveData<List<ChatItem>> {
-        return
+        return mutableLiveData()
     }
 
     private val chatRepository =  ChatRepository
