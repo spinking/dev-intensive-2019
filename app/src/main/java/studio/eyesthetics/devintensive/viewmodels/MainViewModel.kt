32bdb4295f596b7dea3f2729db7d04caa267e85a
Spinking <spinking@mail.ru>
2019-08-29 09:13:31 +0400

    implemented search for chat groups

--- app/src/main/java/studio/eyesthetics/devintensive/viewmodels/MainViewModel.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/viewmodels/MainViewModel.kt
@@ -1,10 +1,12 @@
 package ru.skillbranch.devintensive.viewmodels
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import ru.skillbranch.devintensive.extensions.mutableLiveData
 import ru.skillbranch.devintensive.models.data.ChatItem
+import ru.skillbranch.devintensive.models.data.UserItem
 import ru.skillbranch.devintensive.repositories.ChatRepository
 /**
  * Created by BashkatovSM on 26.08.2019
@@ -18,8 +20,24 @@ class MainViewModel: ViewModel() {
             .sortedBy { it.id.toInt() }
     }
 
-    fun getChatData() : LiveData<List<ChatItem>> {
+    /*fun getChatData() : LiveData<List<ChatItem>> {
         return chats
+    }*/
+
+    fun getChatData() : LiveData<List<ChatItem>> {
+        val result = MediatorLiveData<List<ChatItem>>()
+
+        val filterF = {
+            val queryStr = query.value!!
+            val searchChats = chats.value!!
+
+            result.value = if(queryStr.isEmpty()) searchChats
+            else searchChats.filter { it.title.contains(queryStr, true) }
+        }
+
+        result.addSource(chats) { filterF.invoke() }
+        result.addSource(query) {filterF.invoke()}
+        return result
     }
 
     fun addToArchive(chatId: String) {
