f15381cdd881cadc1d73f3cf900f0f3a1976a23e
Spinking <spinking@mail.ru>
2019-08-29 08:17:33 +0400

    added archive environment

---
+++ app/src/main/java/studio/eyesthetics/devintensive/ui/archive/ArchiveActivity.kt
@@ -0,0 +1,13 @@
+package ru.skillbranch.devintensive.ui.archive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.skillbranch.devintensive.R
+
+class ArchiveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_archive)
+    }
+}
---
+++ app/src/main/java/studio/eyesthetics/devintensive/viewmodels/ArchiveViewModel.kt
@@ -0,0 +1,7 @@
+package ru.skillbranch.devintensive.viewmodels
+
+/**
+ * Created by BashkatovSM on 29.08.2019
+ */
+class ArchiveViewModel {
+}
