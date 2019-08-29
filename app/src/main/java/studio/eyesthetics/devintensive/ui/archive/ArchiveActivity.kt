41d710d9b8856272d677945d92745e2e238d1f43
Spinking <spinking@mail.ru>
2019-08-29 10:00:47 +0400

    implemented ArchiveViewModel

--- app/src/main/java/studio/eyesthetics/devintensive/ui/archive/ArchiveActivity.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/ui/archive/ArchiveActivity.kt
@@ -1,13 +1,63 @@
 package ru.skillbranch.devintensive.ui.archive
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_archive.*
+import kotlinx.android.synthetic.main.activity_main.toolbar
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.ui.adapters.ChatAdapter
+import ru.skillbranch.devintensive.ui.adapters.ChatItemTouchHelperCallback
+import ru.skillbranch.devintensive.viewmodels.ArchiveViewModel
 
 class ArchiveActivity : AppCompatActivity() {
+
+    private lateinit var chatAdapter: ChatAdapter
+    private lateinit var viewModel: ArchiveViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_archive)
+        initToolbar()
+        initViews()
+        initViewModel()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+    }
+
+    private fun initViews() {
+        chatAdapter = ChatAdapter{
+            Snackbar.make(rv_archive_list, "Click on ${it.title}", Snackbar.LENGTH_LONG).show()
+        }
+        //ДЗ кастом материал декоратор time: 1:13 tutorial 5
+        val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        val touchCallback = ChatItemTouchHelperCallback(chatAdapter) {
+            viewModel.restoreFromArchive(it.id)
+
+            Snackbar.make(rv_archive_list, "Вы точно хотите убрать ${it.title} из архива?", Snackbar.LENGTH_LONG)
+                .setAction("Нет") { _ -> viewModel.addToArchive(it.id)}
+                .show()
+        }
+        val touchHelper = ItemTouchHelper(touchCallback)
+        touchHelper.attachToRecyclerView(rv_archive_list)
+
+        with(rv_archive_list) {
+            adapter = chatAdapter
+            layoutManager = LinearLayoutManager(this@ArchiveActivity)
+            addItemDecoration(divider)
+        }
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(ArchiveViewModel::class.java)
+        viewModel.getChatData().observe(this, Observer { chatAdapter.updateData(it) })
     }
 }
--- app/src/main/java/studio/eyesthetics/devintensive/viewmodels/ArchiveViewModel.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/viewmodels/ArchiveViewModel.kt
@@ -1,7 +1,56 @@
 package ru.skillbranch.devintensive.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import ru.skillbranch.devintensive.extensions.mutableLiveData
+import ru.skillbranch.devintensive.models.data.ChatItem
+import ru.skillbranch.devintensive.repositories.ChatRepository
+
 /**
  * Created by BashkatovSM on 29.08.2019
  */
-class ArchiveViewModel {
+class ArchiveViewModel : ViewModel() {
+
+    private val query = mutableLiveData("")
+    private val chatRepository =  ChatRepository
+    private val chats = Transformations.map(chatRepository.loadChats()){ chats ->
+        return@map chats.filter { !it.isArchived }
+            .map { it.toChatItem() }
+            .sortedBy { it.id.toInt() }
+    }
+
+    fun getChatData() : LiveData<List<ChatItem>> {
+        val result = MediatorLiveData<List<ChatItem>>()
+
+        val filterF = {
+            val queryStr = query.value!!
+            val searchChats = chats.value!!
+
+            result.value = if(queryStr.isEmpty()) searchChats
+            else searchChats.filter { it.title.contains(queryStr, true) }
+        }
+
+        result.addSource(chats) { filterF.invoke() }
+        result.addSource(query) {filterF.invoke()}
+        return result
+    }
+
+    fun addToArchive(chatId: String) {
+        val chat = chatRepository.find(chatId)
+        chat ?: return
+        chatRepository.update(chat.copy(isArchived = true))
+    }
+
+    fun restoreFromArchive(chatId: String) {
+        val chat = chatRepository.find(chatId)
+        chat ?: return
+        chatRepository.update(chat.copy(isArchived = false))
+    }
+
+    fun handleSearchQuery(text: String) {
+        query.value = text
+    }
+
 }
