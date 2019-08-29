9ed4bca7b323b8f24ebb236d4ae74d5f68666966
Spinking <spinking@mail.ru>
2019-08-29 10:08:40 +0400

    fixed archive filtering

--- app/src/main/java/studio/eyesthetics/devintensive/viewmodels/ArchiveViewModel.kt
+++ app/src/main/java/studio/eyesthetics/devintensive/viewmodels/ArchiveViewModel.kt
@@ -16,7 +16,7 @@ class ArchiveViewModel : ViewModel() {
     private val query = mutableLiveData("")
     private val chatRepository =  ChatRepository
     private val chats = Transformations.map(chatRepository.loadChats()){ chats ->
-        return@map chats.filter { !it.isArchived }
+        return@map chats.filter { it.isArchived }
             .map { it.toChatItem() }
             .sortedBy { it.id.toInt() }
     }
